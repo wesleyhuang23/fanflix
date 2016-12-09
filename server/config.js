d2128500b8ce82e6a51f42d795632008a5a670f5
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-12-08 20:20:33 -0700

    localhost change

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   facebookId: '344613079227778',
   facebookSecret: 'b50fc84265df0ecb4409094d90b3d919',
-  baseDomian: 'http://localhost:3000'
+  baseDomian: 'https://fanflix.herokuapp.com'
 };
