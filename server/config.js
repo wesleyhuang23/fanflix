24fcad214589a1a057d6d5f7ac6566b50c1f83ba
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-12-09 12:24:12 -0700

    fanflixclub in config

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   facebookId: '344613079227778',
   facebookSecret: 'b50fc84265df0ecb4409094d90b3d919',
-  baseDomian: 'https://fanflix.herokuapp.com'
+  baseDomian: 'https://www.fanflix.club'
   // https://fanflix.herokuapp.com
   //https://www.fanflix.club
 };
