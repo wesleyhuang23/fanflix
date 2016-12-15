cd2802c6f3eaee4adfa4507092554d38de7f5e1f
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-12-15 15:47:34 -0700

    updated config

--- server/server_config.js
+++ server/server_config.js
@@ -1,4 +1,4 @@
 module.exports = {
   port: 3000,
-  dbString:no 'postgres://vvafxlns:dkMjkdh9Psj4n4ReIcWY-Oa_jQ9P1DaV@elmer.db.elephantsql.com:5432/vvafxlns'
+  dbString: 'postgres://vvafxlns:dkMjkdh9Psj4n4ReIcWY-Oa_jQ9P1DaV@elmer.db.elephantsql.com:5432/vvafxlns'
 };
