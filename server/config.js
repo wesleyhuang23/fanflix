f5161a8c88656da62683f16c76ecf5f389d2cb01
wesleyhuang23 <wesleyhuang23@gmail.com>
2016-12-15 15:55:31 -0700

    change for heroku

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  facebookId: '344613079227778',
+  facebookSecret: 'b50fc84265df0ecb4409094d90b3d919',
+  baseDomian: 'http://www.fanflix.club'
+  // https://fanflix.herokuapp.com
+  //https://www.fanflix.club
+};
